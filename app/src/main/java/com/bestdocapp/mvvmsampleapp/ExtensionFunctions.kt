34f71595bbe99764b6d3512234a7f513964c769d
clintpaul <clintpaul89@gmail.com>
2020-05-11 23:47:13 +0530

    Added login functionality with validation check using local functions and added view binding as well

---
+++ app/src/main/java/com/bestdocapp/mvvmsampleapp/ExtensionFunctions.kt
@@ -0,0 +1,9 @@
+package com.bestdocapp.mvvmsampleapp
+import android.widget.Toast
+
+/*https://www.youtube.com/watch?v=-ejajxJIhZk
+Watch this video to know more about Extension functions in Kotlin */
+fun String.showToast() = Toast.makeText(
+    MyApplication.instance.applicationContext,
+    this, Toast.LENGTH_SHORT
+).show()
