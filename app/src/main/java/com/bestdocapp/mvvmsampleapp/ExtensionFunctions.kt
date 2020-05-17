80d4bd4f64d900c80b84654adf9eda74942527e2
clintpaul <clintpaul89@gmail.com>
2020-05-17 18:56:38 +0530

    changed the comment for stops lottie animation extension function

--- app/src/main/java/com/bestdocapp/mvvmsampleapp/ExtensionFunctions.kt
+++ app/src/main/java/com/bestdocapp/mvvmsampleapp/ExtensionFunctions.kt
@@ -27,7 +27,9 @@ fun LottieAnimationView.startLottieAnimation() {
 }
 
 
-// Stops the lottie Animation
+/**
+Stops the lottie animation
+ */
 fun LottieAnimationView.stopLottieAnimation() {
     this.visibility = View.GONE
     this.cancelAnimation()
